18ba944c7aa742de77a6eeae8fb0a89e967dd1a0
SunLeL <871705940@qq.com>
2021-04-05 15:41:33 +0800

    add index and bootstrap

--- src/main/java/com/slx/community/controller/IndexController.java
+++ src/main/java/com/slx/community/controller/IndexController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";//此时程序会自动去templates目录下寻找hello.html文件运行
+        return "index";//此时程序会自动去templates目录下寻找index.html文件运行
     }
 }
