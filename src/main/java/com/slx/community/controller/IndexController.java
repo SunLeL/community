7a3dbfa253d525765b9d25da7e3674ffd85b6638
SunLeL <871705940@qq.com>
2021-04-05 15:42:52 +0800

    index

--- src/main/java/com/slx/community/controller/IndexController.java
+++ src/main/java/com/slx/community/controller/IndexController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
 
     @GetMapping("/")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
+    public String index(){
+//        model.addAttribute("name",name);
         return "index";//此时程序会自动去templates目录下寻找index.html文件运行
     }
 }
